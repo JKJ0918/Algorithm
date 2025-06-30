08693c88626d461da8d46e5804087a19f4d89243
JKJ0918 <whrjswo123@naver.com>
2025-06-30 15:31:49 +0900

    [Bronze V] Title: A+B, Time: 180 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        
-        System.out.print(A+B);
-        
-        sc.close();
-      }
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		// 10 20 -> 10 20 띄어쓰기 알아서 구분됨
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+
 }
