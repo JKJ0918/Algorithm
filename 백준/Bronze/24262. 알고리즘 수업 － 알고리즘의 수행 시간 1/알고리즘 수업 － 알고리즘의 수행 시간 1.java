7278527e6120587315f2faf8419318fac1f22f02
JKJ0918 <whrjswo123@naver.com>
2025-07-03 14:48:37 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// 알고리즘 수업 - 알고리즘의 수행 시간 1
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(1);
+		System.out.println(0); // T(n) = 1 = n^0 최고차항은 '0'
+
+	}// main
+
+}
