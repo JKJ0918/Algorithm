a05cae925d53f275d0d100f3f64c21e80361e8c9
JKJ0918 <whrjswo123@naver.com>
2025-07-13 23:04:37 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// 나머지
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		sb.append((A+B)%C+"\n");
+		sb.append(((A%C) + (B%C))%C+"\n");
+		sb.append((A*B)%C+"\n");
+		sb.append(((A%C) * (B%C))%C+"\n");
+		
+		System.out.println(sb);
+
+		
+		
+	}// end main
+
+}
