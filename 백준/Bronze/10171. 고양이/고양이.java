5c631ae4c4d3cd229d3416bacb4d88eec70e7000
JKJ0918 <whrjswo123@naver.com>
2025-07-15 11:27:53 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// 고양이
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\\    /\\"+"\n");
+		sb.append(" )  ( ')"+"\n");
+		sb.append("(  /  )"+"\n");
+		sb.append(" \\(__)|");
+		
+		System.out.println(sb);
+
+	}
+
+}
