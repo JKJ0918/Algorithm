d6792753d40a8e5e0374a851c4e040abf3b71669
JKJ0918 <whrjswo123@naver.com>
2025-07-15 11:30:39 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// 개
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|"+"\n");
+		sb.append("|q p|   /}"+"\n");
+		sb.append("( 0 )\"\"\"\\"+"\n");
+		sb.append("|\"^\"`    |"+"\n");
+		sb.append("||_/=\\\\__|"+"\n");
+		
+		System.out.println(sb);
+
+	}
+
+}
